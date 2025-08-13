7835b3dc9eba66428b56453100c6fed43016a6bc
Lindsay Holmwood <lindsay@holmwood.id.au>
2025-08-14 02:33:04 +1000

    chore: make it work in batches

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { protectDynamoDB } from "@cipherstash/protect-dynamodb";
 import { protect, csColumn, csTable } from "@cipherstash/protect";
 import { PutCommand, GetCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, BatchWriteItemCommand } from "@aws-sdk/client-dynamodb";
 
 // Define your protected table schema
 const users = csTable("users", {
@@ -19,35 +19,81 @@ const protectDynamo = protectDynamoDB({
 });
 
 // Encrypt and store a user
-const user = {
-  email: "user@example.com",
-};
+const exampleUsers = [
+  { email: "alice@example.com" },
+  { email: "bob@company.org" },
+  { email: "carol@tech.io" },
+  { email: "david@startup.com" },
+  { email: "eve@university.edu" },
+  { email: "frank@consulting.biz" },
+  { email: "grace@design.studio" },
+  { email: "henry@finance.net" },
+  { email: "ivy@marketing.agency" },
+  { email: "jack@development.dev" },
+  { email: "lea@ux.design" },
+  { email: "joe@backend.systems" },
+  { email: "ivy@frontend.ui" },
+  { email: "max@devops.cloud" },
+  { email: "ava@ml.algorithms" },
+  { email: "jim@database.admin" },
+  { email: "eva@api.gateway" },
+  { email: "leo@microservices.arch" },
+  { email: "nora@blockchain.crypto" },
+  { email: "owen@quantum.computing" },
+  { email: "sara@edge.computing" },
+  { email: "will@serverless.lambda" },
+  /*
+  { email: "tina@container.docker" },
+  { email: "gary@kubernetes.cluster" },
+  { email: "lara@monitoring.observability" },
+  { email: "kyle@logging.metrics" },
+  { email: "nina@testing.qa" },
+  { email: "dean@automation.ci" },
+  { email: "rosa@deployment.cd" },
+  { email: "felix@infrastructure.iac" },
+  { email: "gina@networking.vpc" },
+  { email: "noah@storage.s3" },
+  { email: "vera@cdn.cloudfront" },
+  { email: "security@compliance.audit" },
+  { email: "pete@backup.recovery" },
+  { email: "cora@scaling.performance" },
+  { email: "jake@optimization.speed" },
+  { email: "luna@cache.redis" },
+  */
+];
 
-const encryptResult = await protectDynamo.encryptModel(user, users);
+const encryptResult = await protectDynamo.bulkEncryptModels(exampleUsers, users);
 if (encryptResult.failure) {
   throw new Error(`Failed to encrypt user: ${encryptResult.failure.message}`);
 }
+
+const items = {
+  RequestItems: {
+    users: encryptResult.data.map((i) => {
+      return {
+        PutRequest: {
+          Item: { email__hmac: { S: i.email__hmac }, email__source: { S: i.email__source } },
+        },
+      };
+    }),
+  },
+};
 
 // Setup a DynamoDB client
 const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 console.log(encryptResult.data);
+console.log(items);
 
 // Store in DynamoDB
-const put = new PutCommand({
-  TableName: users.tableName,
-  Item: encryptResult.data,
-});
-console.log(put);
-await docClient.send(put);
-
-process.exit(0);
+const batchWrite = new BatchWriteItemCommand(items);
+await docClient.send(batchWrite);
 
 // Create search terms for querying
 const searchTermsResult = await protectDynamo.createSearchTerms([
   {
-    value: "user@example.com",
+    value: exampleUsers[Math.floor(Math.random() * exampleUsers.length)].email,
     column: users.email,
     table: users,
   },
